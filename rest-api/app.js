1b74d5cba2b853056cdbee87764350f6c23fd094
ahmet <ahmetylm327@gmail.com>
2026-03-23 21:36:31 +0300

    fix: frontend icin cors ayari ve bagimliliklar eklendi

--- rest-api/app.js
+++ rest-api/app.js
@@ -1,8 +1,10 @@
 const express = require('express');
+const cors = require('cors');
 require('./app_api/models/db');
 const apiRouter = require('./app_api/routers/index');
 
 const app = express();
+app.use(cors());
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
