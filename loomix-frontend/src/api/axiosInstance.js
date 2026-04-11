e004ae18dc2d93f604ea8eaf76fba0bb694da688
ahmet <ahmetylm327@gmail.com>
2026-04-11 19:56:23 +0300

    link verildi

--- loomix-frontend/src/api/axiosInstance.js
+++ loomix-frontend/src/api/axiosInstance.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const axiosInstance = axios.create({
     // baseURL: 'https://loomix-xlp4.onrender.com/api', 
-    baseURL: 'http://localhost:9000/api',
+    baseURL: 'https://loomix-backend.onrender.com/api',
     headers: {
         'Content-Type': 'application/json'
     }
